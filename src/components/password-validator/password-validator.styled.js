bd127228fcc78621dae563eabb9a8186cce7e8c4
keyner07 <47069469+keyner07@users.noreply.github.com>
2023-11-12 09:57:27 -0400

    Create requirement list and requirement item styled components

--- src/components/password-validator/password-validator.styled.js
+++ src/components/password-validator/password-validator.styled.js
@@ -1,5 +1,5 @@
 import styled from "@emotion/styled";
-import { TextField, Box } from '@mui/material';
+import { TextField, Box, List, ListItem, Typography } from '@mui/material';
 
 export const PasswordField = styled(TextField)(() => ({
     marginBottom: '10px',
@@ -12,4 +12,23 @@ export const Container = styled(Box)(() => ({
     flexDirection: 'column',
     alignItems: "center",
     fontFamily: 'Arial'
+}));
+
+export const RequirementList = styled(List)(() => ({
+    marginBottom: '10px',
+    padding: '8px',
+    fontSize: '16px'
+}));
+
+export const RequirementItem = styled(ListItem, { shouldForwardProp: (prop) => prop !== 'isValid'})(({ isValid }) => ({
+    color: isValid ? 'green' : 'red'
+}));
+
+export const Title = styled(Typography)(() => ({
+    fontFamily: 'Arial',
+    fontStyle: 'normal',
+    fontWeight: '400',
+    fontSize: '24px',
+    lineHeight: '42px',
+    color: 'black',
 }));
--- src/components/password-validator/password.validator.component.js
+++ src/components/password-validator/password.validator.component.js
@@ -1,17 +1,23 @@
 import React, { useState } from 'react';
-import { Container, PasswordField } from './password-validator.styled';
+import { Container, PasswordField, RequirementItem, RequirementList, Title } from './password-validator.styled';
 
-const PasswordValidator = () => {
+const PasswordValidator = ({ options= [] }) => {
     const [password, setPassword] = useState('');
 
     return (
         <Container>
+            <Title>Password Component</Title>
             <PasswordField
                 type="password"
                 placeholder="Enter your password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
             ></PasswordField>
+            <RequirementList>
+                {options.map((option, index) => (
+                    <RequirementItem key={index}></RequirementItem>
+                ))}
+            </RequirementList>
         </Container>
     )
 };
